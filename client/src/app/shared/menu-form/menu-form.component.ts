64406a7ef0e71bd836d5f0f28ab293f8962411d6
Branislav Jevtic <branislav.jevtic@rt-rk.com>
2021-07-01 10:30:24 +0200

    [FRONT] Menu version v0.3

--- client/src/app/shared/menu-form/menu-form.component.ts
+++ client/src/app/shared/menu-form/menu-form.component.ts
@@ -7,43 +7,28 @@ import { Dish, Menu } from 'src/app/models/Food.model';
   styleUrls: ['./menu-form.component.css']
 })
 export class MenuFormComponent implements OnInit {
-  @Input('myDishes') dishes: Dish[];
   @Output('menu') onMenu: EventEmitter<Menu> = new EventEmitter();
 
-  allDishes: Dish[] = [];
 
   menu: Menu = {
     id: -1,
     name: "",
     dishes: []
   };
-  selectedDish: Dish;
+  private menuFile: File = undefined;
 
   constructor() { }
 
   ngOnInit(): void {
-    if(this.dishes !== undefined){
-      for(const d of this.dishes){
-        this.allDishes.push(d);
-      }
-    }
+    
   }
 
-  onDishSelect(){
-    for(let i = 0; i < this.allDishes.length; ++i){
-      if(this.selectedDish.id == this.allDishes[i].id){
-        this.menu.dishes.push(this.allDishes[i]);
-        this.allDishes.splice(i, 1);
-        break;
-      }
-    }
 
-    this.selectedDish = null;
+  onFileSelect($event){
+
   }
 
   removeDish(index: number){
-    this.allDishes.push(this.menu.dishes[index]);
-    this.menu.dishes.splice(index, 1);
   }
 
   onConfirm(){
